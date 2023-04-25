3460d5ff6aa1a1de140fa813fed3e7179001fbe6
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2023-04-25 14:12:27 +0530

    Update server.js
    update port to 5000

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -8,7 +8,7 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 
 const connectDB = require("./Configs/config");
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
