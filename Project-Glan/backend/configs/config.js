38617d7aebf554ed58106b1e2eea969f877ff5cd
Shehan <shehanjr99@gmail.com>
2023-05-02 04:54:13 +0530

    Resolved the issue express module and the error in config file

--- Project-Glan/backend/configs/config.js
+++ Project-Glan/backend/configs/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
+        const conn = await mongoose.connect('mongodb+srv://GlanInternational:sewaka1234@glandb.yluz4kb.mongodb.net/GlanDB?retryWrites=true&w=majority');
     } catch (err) {
         console.error(err);
     }
