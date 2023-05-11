145e8203a012f9e392604c27ab1ac8f93072c718
Supun <supunvirajith21@gmail.com>
2023-05-11 19:07:31 +0530

    modified vehicle report

--- Project-Glan/frontend/src/pages/TaskReport.jsx
+++ Project-Glan/frontend/src/pages/TaskReport.jsx
@@ -1,12 +1,22 @@
 import { useReactToPrint } from "react-to-print";
 import { connect } from "react-redux";
 import { FetchTaskList, RemoveTask } from "../redux/actions/TaskAction";
-import { useEffect , useState , useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import Header_bar_menu from "../components/Header_bar/Header_bar_manu";
+import "../pages/Content.css";
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FiEdit } from "react-icons/fi";
+import { FiTrash2 } from "react-icons/fi";
+import { BiSearchAlt } from "react-icons/bi";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 const TaskListing = (props) => {
-    const componentPDF = useRef();
+  const [searchTerm, setSearchTerm] = useState("");
+  const componentPDF = useRef();
 
   useEffect(() => {
     props.loadtask();
@@ -35,49 +45,98 @@ const TaskListing = (props) => {
     </div>
   ) : (
     <div>
-      <div className="card">
-        <div className="card-header">
-          <Link to={"/task/add"} className="btn btn-success">
-            Create New Task [+]
-          </Link>
-        </div>
-        <div className="card-body">
-        <div ref={componentPDF} style = {{width: "100%"}}>
-          <table className="table table-bordered">
-            <thead className="bg-dark text-white">
+
+      <div>
+        {" "}
+        <Header_bar_menu fun1="Dashboard" fun2="Task" fun7="Report" />
+      </div>
+      <div className="search">
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon1">
+            <BiSearchAlt />
+          </InputGroup.Text>
+          <Form.Control
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="basic-addon1"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </InputGroup>
+      </div>
+      <div class="page_sub_header">
+        <t class="sub_header_topic">View Tasks</t>
+        <Link to="/task/add" className="page_link">
+          Create
+        </Link>
+      </div>
+
+
+
+
+
+      <div className="Content">
+        <div ref={componentPDF} style={{ width: "100%" }}>
+          <h4>Glan International Pvt Limited</h4>
+          <h6>No.551,Mihindu Mawatha,Malabe,Sri Lanaka</h6>
+          <h6>glaninternational@gmail.com</h6>
+          <center><h4>Task List Report</h4></center>
+          <hr />
+          <Table striped hover className="table">
+            <thead className="theaderManuf">
               <tr>
-                <td>Obj Id</td>
+
                 <td>Task Id</td>
                 <td>Output Item Name</td>
                 <td>Output Item Quantity</td>
                 <td>Production Supervisor</td>
                 <td>Start Date</td>
                 <td>End Date</td>
-                
+                <td>Status</td>
+
               </tr>
             </thead>
-            <tbody>
+            <tbody className="tbodyManuf">
               {props.task.tasklist &&
-                props.task.tasklist.map((item) => (
-                  <tr key={item._id}>
-                    <td>{item._id}</td>
-                    <td>{item.tId}</td>
-                    <td>{item.iName}</td>
-                    <td>{item.iQty}</td>
-                    <td>{item.prodSupe}</td>
-                    <td>{item.sDate}</td>
-                    <td>{item.eDate}</td>
-                    
-                  </tr>
-                ))}
+
+                props.task.tasklist
+                  .filter(
+                    (item) =>
+                      item.tId
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      item.iName
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      item.sDate
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      item.prodSupe
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                  )
+                  .map((item) => (
+                    <tr>
+
+                      <td>{item.tId}</td>
+                      <td>{item.iName}</td>
+                      <td>{item.iQty}</td>
+                      <td>{item.prodSupe}</td>
+                      <td>{item.sDate}</td>
+                      <td>{item.eDate}</td>
+                      <td>{item.tState}</td>
+
+                    </tr>
+                  ))}
             </tbody>
-          </table>
-          </div>
+          </Table>
         </div>
         <button className="btn btn-success" onClick={generatePDF}>PDF Download</button>
-
       </div>
+
+
     </div>
+
   );
 };
 
--- Project-Glan/frontend/src/pages/VehicleReport.jsx
+++ Project-Glan/frontend/src/pages/VehicleReport.jsx
@@ -1,5 +1,5 @@
 import { useReactToPrint } from "react-to-print"
-import { useEffect, useRef,useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { FetchVehicleList, RemoveVehicle } from "../redux/actions/VehicleAction";
 import { connect } from "react-redux";
@@ -16,39 +16,39 @@ import InputGroup from "react-bootstrap/InputGroup";
 
 
 const VehicleList = (props) => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const conponentPDF = useRef();
-    useEffect(() => {
-        props.loadvehicle();
-    }, []);
+  const [searchTerm, setSearchTerm] = useState("");
+  const conponentPDF = useRef();
+  useEffect(() => {
+    props.loadvehicle();
+  }, []);
 
 
-    const generatePDF = useReactToPrint({
-        content: () => conponentPDF.current,
-        documentTitle: "itemlists",
-        onAfterPrint: () => alert("Data saved in PDF"),
-      });
+  const generatePDF = useReactToPrint({
+    content: () => conponentPDF.current,
+    documentTitle: "itemlists",
+    onAfterPrint: () => alert("Data saved in PDF"),
+  });
 
-    const handleDelete = (code) => {
-        if (window.confirm(`Remove task ${code} ?`)) {
-            props.removevehicle(code);
-            props.loadvehicle();
-            toast.success("Vehicle removed successfully");
-        }
-    };
+  const handleDelete = (code) => {
+    if (window.confirm(`Remove task ${code} ?`)) {
+      props.removevehicle(code);
+      props.loadvehicle();
+      toast.success("Vehicle removed successfully");
+    }
+  };
 
-    return props.vehicle.loading ? (
-        <div>
-            <h2>Loading data...</h2>
-        </div>
-    ) : props.vehicle.errmessage ? (
-        <div>
-            <h2>{props.vehicle.errmessage}</h2>
-        </div>
-    ) : (
-        <div>
+  return props.vehicle.loading ? (
+    <div>
+      <h2>Loading data...</h2>
+    </div>
+  ) : props.vehicle.errmessage ? (
+    <div>
+      <h2>{props.vehicle.errmessage}</h2>
+    </div>
+  ) : (
+    <div>
 
-<div>
+      <div>
         {" "}
         <Header_bar_vehi
           fun1="Dashboard"
@@ -75,7 +75,7 @@ const VehicleList = (props) => {
         </InputGroup>
       </div>
       <div class="page_sub_header">
-        <t class="sub_header_topic"><h1>View Vehicle User</h1></t>
+        <t class="sub_header_topic"><h3>View Vehicle User</h3></t>
         <Link to="/vehicle/add" className="page_link">
           Create
         </Link>
@@ -84,74 +84,77 @@ const VehicleList = (props) => {
 
 
       <div className="Content">
-            
-            
-                
-                
-                <div ref={conponentPDF} style={{ width: "100%" }}>
-                <h3>Glan International Pvt Limited</h3>
-            <h4>Vehicle Item List</h4>
-                <Table striped hover className="table">
-          <thead className="theader">
-            <tr>
-              
-              <td>Vehicle Type</td>
-              <td>Vehicle NumPlate </td>
-              <td>Vehicle Insurance</td>
-              <td>Vehicle Capacity</td>
-              <td>Vehicle Status</td>
-              
-            </tr>
-          </thead>
-          <tbody className="tbody">
-            {props.vehicle.vehiclelist &&
-              props.vehicle.vehiclelist
-                .filter(
-                  (item) =>
-                    item.insurance
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase()) ||
-                    item.vType
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase()) ||
-                    item.vStatus
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase())
-                )
-                .map((item) => (
-                  <tr>
-                    
-                    <td>{item.vType}</td>
-                    <td>{item.numPlate}</td>
-                    <td>{item.insurance}</td>
-                    <td>{item.capacity}</td>
-                    <td>{item.vStatus}</td>
-                    
-                  </tr>
-                ))}
-          </tbody>
-        </Table>
-                    </div>
-                    <button className="btn btn-success" onClick={generatePDF}>
-            PDF DOWNLOADS
-          </button>
-                </div>
-            
+
+
+
+
+        <div ref={conponentPDF} style={{ width: "100%" }}>
+          <h4>Glan International Pvt Limited</h4>
+          <h6>No.551,Mihindu Mawatha,Malabe,Sri Lanaka</h6>
+          <h6>glaninternational@gmail.com</h6>
+          <center><h4>Vehicle List Report</h4></center>
+          <hr />
+          <Table striped hover className="table">
+            <thead className="theader">
+              <tr>
+
+                <td>Vehicle Type</td>
+                <td>Vehicle NumPlate </td>
+                <td>Vehicle Insurance</td>
+                <td>Vehicle Capacity</td>
+                <td>Vehicle Status</td>
+
+              </tr>
+            </thead>
+            <tbody className="tbody">
+              {props.vehicle.vehiclelist &&
+                props.vehicle.vehiclelist
+                  .filter(
+                    (item) =>
+                      item.insurance
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      item.vType
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                      item.vStatus
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                  )
+                  .map((item) => (
+                    <tr>
+
+                      <td>{item.vType}</td>
+                      <td>{item.numPlate}</td>
+                      <td>{item.insurance}</td>
+                      <td>{item.capacity}</td>
+                      <td>{item.vStatus}</td>
+
+                    </tr>
+                  ))}
+            </tbody>
+          </Table>
         </div>
-    );
+        <button className="btn btn-success" onClick={generatePDF}>
+          PDF DOWNLOADS
+        </button>
+      </div>
+
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        vehicle: state.vehicle,
-    };
+  return {
+    vehicle: state.vehicle,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        loadvehicle: () => dispatch(FetchVehicleList()),
-        removevehicle: (code) => dispatch(RemoveVehicle(code)),
-    };
+  return {
+    loadvehicle: () => dispatch(FetchVehicleList()),
+    removevehicle: (code) => dispatch(RemoveVehicle(code)),
+  };
 
 
 }
